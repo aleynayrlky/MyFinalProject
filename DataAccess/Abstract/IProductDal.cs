58bbca9431bf963698a136c24fc1c2835ebd6151
Aleyna Yerlikaya <aleynayrlky@gmail.com>
2021-05-01 11:42:37 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //produt'la ilgili veri tabanında yapılacak operasyonları yazarız
+    public interface IProductDal
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategory(int categoryId);
+    }
+}
